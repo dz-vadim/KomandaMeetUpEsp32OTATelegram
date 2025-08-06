fa86e816cc4e04e93f778e5aa414efc34a5f6846
VADYM DZIUBA <46889822+dz-vadim@users.noreply.github.com>
2025-08-06 04:03:42 +0300

    Update telegram_api.h

--- include/telegram_api.h
+++ include/telegram_api.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <Arduino.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+#include "secrets.h"
+
+// Глобальні об'єкти/змінні, які визначені в telegram_api.cpp
+extern WiFiClientSecure tgClient;
+extern UniversalTelegramBot bot;
+extern int lastProcessedId;
 
 void telegramInit();
 bool sendTelegram(const String &text);
